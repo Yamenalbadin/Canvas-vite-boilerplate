b0378fd2bc0e64193d227e7e7bee12156b89e500
YamenAlbadin <yamengfx@gmail.com>
2024-09-24 09:23:35 +0100

    update

--- canvas.js
+++ canvas.js
@@ -3,33 +3,24 @@ import { randomIntFromRange, randomColor, distance } from './utils'
 const dpr = 2
 let canvasWidth = 0
 let canvasHeight = 0
-const text = 'HTML CANVAS BOILERPLATE'
+const text = 'mag'
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
-
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-const setCanvasDimensions = () => {
-  canvasWidth = innerWidth
-  canvasHeight = innerHeight
-  canvas.width = canvasWidth * dpr
-  canvas.height = canvasHeight * dpr
-  canvas.style.width = canvasWidth + "px"
-  canvas.style.height = canvasHeight + "px"
-}
-
-setCanvasDimensions()
-
+const magnificationFactor = 2
+const magnificationRadius = 1
 const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 }
-
+const magPosition = {
+  x: 0,
+  y: 0
+}
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
   mouse.x = event.clientX
   mouse.y = event.clientY
+  handleMouseMove(event)
 })
 
 addEventListener('resize', () => {
@@ -59,6 +50,59 @@ class Object {
   }
 }
 
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+const setCanvasDimensions = () => {
+  canvasWidth = innerWidth
+  canvasHeight = innerHeight
+  canvas.width = canvasWidth * dpr
+  canvas.height = canvasHeight * dpr
+  canvas.style.width = canvasWidth + "px"
+  canvas.style.height = canvasHeight + "px"
+}
+setCanvasDimensions()
+
+
+const drawText = (size) => {
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = colors[0]
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = 'white'
+  c.font = "60px Helvatica"
+  c.textBaseline = "middle"
+  c.textAlign = "center"
+  // c.fillText(text, mouse.x*dpr, mouse.y*dpr)
+  c.fillText(text, canvas.width/2, canvas.height/2)
+}
+
+const magnifyText = (x, y) => {
+  c.save()
+  c.beginPath();
+  c.arc(x, y, magnificationRadius, 0, 2 * Math.PI)
+  c.lineWidth = 1
+  c.globalAlpha = 1
+  c.strokeStyle = 'black'
+  c.stroke()
+  c.clip()
+
+  const sourceWidth = magnificationRadius * 2 * dpr
+  const sourceHeight = magnificationRadius * 2 * dpr
+
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = colors[0]
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = "white"
+  c.font = "60px Helvatica"
+  const X = canvas.width/2 - x
+  const Y = canvas.height/2 - y
+  c.textBaseline = "middle"
+  c.textAlign = "center"
+  c.fillText(text, X, Y)
+  c.restore()
+}
+
+
 // Implementation
 let objects
 function init() {
@@ -69,19 +113,29 @@ function init() {
   }
 }
 
+const handleMouseMove = (e) => {
+  const rect = canvas.getBoundingClientRect()
+  const x = e.clientX - rect.left
+  const y = e.clientY - rect.top
+  magPosition.x = x
+  magPosition.y = y
+  drawText(12)
+  if(x > magnificationRadius && y > magnificationRadius && x < rect.width - magnificationRadius && y < rect.height - magnificationRadius){
+    magnifyText(x, y)
+  }
+}
+
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
-  c.font = "60px Helvatica"
-  c.textBaseline = "middle"
-  c.textAlign = "left"
-
-  c.fillText(text, mouse.x*dpr, mouse.y*dpr)
+  // drawText(12)
   // objects.forEach(object => {
   //  object.update()
   // })
 }
 
 init()
-animate()
+drawText(12)
+
+// animate()
